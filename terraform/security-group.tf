c3d874af69fe35a5cc08c52acff54fc4475981a2
Zaksk <zakariamar.99@gmail.com>
2025-02-20 10:46:35 +0000

    Fix .tf files

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -36,7 +36,7 @@ resource "aws_security_group_rule" "app_port_ingress" {
 resource "aws_security_group_rule" "egress" {
   type = "egress"
   from_port = 0
-  to_port = 0
+  to_port = 0 
   protocol = -1
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.http_server_sg.id
